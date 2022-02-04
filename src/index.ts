41f9c030b332aef8190399b780412b2de306895e
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-04 14:26:28 +0800

    Added initialisation of database connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+import 'dotenv/config';
+import { MikroORM } from "@mikro-orm/core";
+import { __dbName__, __prod__, __dbpassword__ } from "./constant";
+import entities from "./entities";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: entities,
+        dbName: __dbName__,
+        type: 'postgresql',
+        password: __dbpassword__,
+        debug: !__prod__
+    });
+}
+
+main().catch(error => {
+    console.error(error);
+});
