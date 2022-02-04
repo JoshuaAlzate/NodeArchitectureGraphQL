3b84c82649edad5ad6ab3e11eb12f74d8009c832
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-04 15:59:41 +0800

    Added migrations path and pattern

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
+import path from "path/posix";
 import { __dbName__, __prod__, __dbpassword__ } from "./constant";
 import entities from "./entities";
 
 export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/
+    },
     entities: entities,
     dbName: __dbName__,
     type: 'postgresql',
