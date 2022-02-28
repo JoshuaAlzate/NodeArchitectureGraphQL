7a1478599f3dce0d2dc0f2ea7dc3b227f702d8ca
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-01 01:01:51 +0800

    Added DB Password to the constant

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,4 @@
 export const __prod__ = process.env.ENVIRONMENT === 'production';
 export const __dbName__ = process.env.DB_NAME;
 export const __dbpassword__ = process.env.DB_PASSWORD;
+export const __port__ = process.env.PORT;
