c466149eba374ad7a18aa81b0472976f60bc9f53
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-02 01:25:34 +0800

    Added User Entity

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: 'text', unique: true })
+  username!: string;
+
+  @Property({ type: 'text' })
+  password!: string;
+
+  @Field(() => Date)
+  @Property({ type: Date, onCreate: () => new Date() })
+  createdAt!: Date
+
+  @Field(() => Date)
+  @Property({ type: Date, nullable: true })
+  updatedAt: Date
+
+}
