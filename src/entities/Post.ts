c376c73857de9b15621650b7c28e489e2df9ddd3
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-01 19:24:08 +0800

    Added GraphQL Types in the Post Fields

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,12 +1,24 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
 
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'text' })
   title!: string;
+
+  @Field(() => Date)
+  @Property({ type: Date, onCreate: () => new Date() })
+  createdAt!: Date
+
+  @Field(() => Date)
+  @Property({ type: Date, nullable: true })
+  updatedAt: Date
 
 }
