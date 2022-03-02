1de44e79abb374eca0a773f8d14c7f0a5cc4f62f
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-03 01:27:09 +0800

    Added Login Function in the User Resolver

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -2,6 +2,8 @@ import argon2 from "argon2";
 import { User } from "../entities/User";
 import { Arg, Ctx, Field, InputType, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
+import { LoginCredentials } from "../types/login-credentials";
+import { UserResponse } from "../types/user-response";
 
 
 @Resolver()
@@ -13,5 +15,18 @@ export class UserResolver {
         const loginCredentials = em.create(User, { username, password });
         await em.persistAndFlush(loginCredentials);
         return loginCredentials;
+    }
+
+    @Query(() => UserResponse)
+    async login(@Arg('credentials') credentials: LoginCredentials, @Ctx() { em }: MikroORM<IDatabaseDriver<Connection>>): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: credentials.username });
+        if(!user) return {
+            errors: [ { message: 'User does not exist', field: 'username'} ]
+        }
+        const isPasswordValid = await argon2.verify(user.password, credentials.password);
+        if(!isPasswordValid) return {
+            errors: [{ message: 'Password is invalid', field: 'password' }]
+        }
+        return { user }
     }
 }
