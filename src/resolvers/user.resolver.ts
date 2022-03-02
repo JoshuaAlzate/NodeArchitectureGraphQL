00ef52d771c3ce98dc9f9ee2198ecaf5150ba304
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-03 00:26:50 +0800

    Added User Resolver

---
+++ src/resolvers/user.resolver.ts
@@ -0,0 +1,25 @@
+import argon2 from "argon2";
+import { User } from "../entities/User";
+import { Arg, Ctx, Field, InputType, Int, Mutation, Query, Resolver } from "type-graphql";
+import { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
+
+
+@InputType()
+class LoginCredentials {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+}
+
+@Resolver()
+export class UserResolver {
+    @Mutation(() => User)
+    async register(@Arg('credentials') credentials: LoginCredentials, @Ctx() { em }: MikroORM<IDatabaseDriver<Connection>>): Promise<User> {
+        credentials.password = await argon2.hash(credentials.password);
+        const { username, password } = credentials;
+        const loginCredentials = em.create(User, { username, password });
+        await em.persistAndFlush(loginCredentials);
+        return loginCredentials;
+    }
+}
