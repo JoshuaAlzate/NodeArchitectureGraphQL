619004826323b9c547386293a5ad69f197646609
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-06 23:06:22 +0800

    Added redisSecret reference in constant

--- src/constant.ts
+++ src/constant.ts
@@ -2,3 +2,4 @@ export const __prod__ = process.env.ENVIRONMENT === 'production';
 export const __dbName__ = process.env.DB_NAME;
 export const __dbpassword__ = process.env.DB_PASSWORD;
 export const __port__ = process.env.PORT;
+export const __redisSecret__ = process.env.REDIS_SECRET || '';
