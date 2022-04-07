f3e5e2b2eeeca112bf98c0af7ba035c2e3d37c37
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-08 01:16:17 +0800

    Added mailer credentials

--- src/constant.ts
+++ src/constant.ts
@@ -4,3 +4,5 @@ export const __dbpassword__ = process.env.DB_PASSWORD;
 export const __port__ = process.env.PORT;
 export const __redisSecret__ = process.env.REDIS_SECRET || '';
 export const COOKIE_NAME = 'qid';
+export const __mailerUser__ = process.env.MAILER_USER;
+export const __mailerPassword__ = process.env.MAILER_PASSWORD;
